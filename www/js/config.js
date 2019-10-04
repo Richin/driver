50577c8671f3db6d1e9e0d1bbf7c742cbdf2dc89
Richin Varghese <richin1990@gmail.com>
2019-10-04 23:08:39 +0530

    haskeychangd

--- www/js/config.js
+++ www/js/config.js
@@ -1,9 +1,9 @@
 /*Driver App Configuration*/
 
-var krms_driver_config ={			
-	'ApiUrl':"",		
+var krms_driver_config ={
+	'ApiUrl':"",
 	'DialogDefaultTitle':"DriverApp",
 	'mapboxToken' : '',
-	'APIHasKey':"",
+	'APIHasKey':"w1o3sdsfvo4eandux30mdr3b70hb4a1fozmd267j",
 	'debug': false
 };
