40618df004e7f501f24438bc345bba313b852fac
Richin Varghese <richin1990@gmail.com>
2019-10-04 23:13:01 +0530

    api url updated

--- www/js/config.js
+++ www/js/config.js
@@ -1,7 +1,7 @@
 /*Driver App Configuration*/
 
 var krms_driver_config ={
-	'ApiUrl':"",
+	'ApiUrl':"http://mobilecity-shopping-delivery.akstech.com.sg/?ng=api/v3/",
 	'DialogDefaultTitle':"DriverApp",
 	'mapboxToken' : '',
 	'APIHasKey':"w1o3sdsfvo4eandux30mdr3b70hb4a1fozmd267j",
